1a10712687a3bff43bf66bbed407b3ed1eb412c7
thomasfowlerFIS <81974837+thomasfowlerFIS@users.noreply.github.com>
2021-05-14 14:13:32 -0600

    Branded for Academy Xi

--- spec/a_tag_with_href_attribute_spec.rb
+++ spec/a_tag_with_href_attribute_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe "An <a> tag with an 'href' attribute" do
     expect(a_tag).to_not be_nil
   end
 
-  it "has the inner text of 'Flatiron School'" do
+  it "has the inner text of 'Academy Xi'" do
     a_tag = parsed_html.search('a').first
 
-    expect(a_tag.text).to eq('Flatiron School')
+    expect(a_tag.text).to eq('Academy Xi')
   end
 
-  it "has the href attribute of 'https://flatironschool.com'" do
+  it "has the href attribute of 'https://academyxi.com'" do
     a_tag = parsed_html.search('a').first
 
-    expect(a_tag.attr('href')).to eq('https://flatironschool.com')
+    expect(a_tag.attr('href')).to eq('https://academyxi.com')
   end
 end
